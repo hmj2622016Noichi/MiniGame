ad9b7048e3027e7019fea7eb2d59d7f7a27b0bfb
hmj2622016Noichi <hmj2622016@stu.o-hara.ac.jp>
2026-06-16 09:52:21 +0900

    a

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -13,7 +13,7 @@ public class Car: MonoBehaviour
 	{
 		transform.position += Vector3.left * speed * Time.deltaTime;
 
-		if (transform.position.x < -700)
+		if (transform.position.x < -750)
 		{
 			SceneManager.LoadScene("ClearScene");
 		}
